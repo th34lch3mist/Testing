9b2a62831f7a52e27181fc69552668644ff640d6
Ankit Tiwari <37219600+th34lch3mist@users.noreply.github.com>
2024-04-09 01:41:25 +0530

    Add files via upload

---
+++ Ankit.py
@@ -0,0 +1,3 @@
+print("Subdomain Takeover by Th3Alch3mist")
+print("Welcome to the webpage after taking over Subdomain!")
+print("\nHere as after taking over i can add or uplaod files or web applications on this vulnerable subdomains without any restrictions")
