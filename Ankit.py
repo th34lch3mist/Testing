236a7abfa66f3c65528ac6632ed4236e3db99095
Ankit Tiwari <37219600+th34lch3mist@users.noreply.github.com>
2025-02-10 19:45:01 +0530

    Delete Ankit.py

--- Ankit.py
+++
@@ -1,3 +0,0 @@
-print("Subdomain Takeover by Th3Alch3mist")
-print("Welcome to the webpage after taking over Subdomain!")
-print("\nHere as after taking over i can add or uplaod files or web applications on this vulnerable subdomains without any restrictions")
